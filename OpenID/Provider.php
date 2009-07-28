921b4378d0bd828d116ce26605b84801e3cc2384
Bill Shupp <hostmaster@shupp.org>
2009-07-27 19:05:50 -0700

    Some more light work on OpenID_Provider

--- OpenID/Provider.php
+++ OpenID/Provider.php
@@ -4,6 +4,13 @@ require_once 'OpenID.php';
 
 class OpenID_Provider
 {
+    protected $message = null;
+    protected $options = array();
+    protected $validModes = array(
+        'checkid_setup',
+        'associate',
+        'checkid_authenticate'
+    );
     protected $validOptions = array(
         'associationTypes',
         'Crypt_DiffieHellman',
@@ -12,30 +19,39 @@ class OpenID_Provider
     public function __construct(OpenID_Message $message,
                                 array $options = array())
     {
+        $this->message = $message;
         $this->setOptions($options);
     }
 
     protected function setOptions(array $options)
     {
-        foreach ($options as $option)
+        foreach ($options as $name => $value)
         {
-            $this->setOption($option);
+            $this->setOption($name, $value);
         }
     }
 
-    protected function setOption($option)
+    protected function setOption($name, $value)
     {
+        if (in_array($name, $this->validOptions)) {
+            $this->options[$name] = $value;
+        }
     }
 
-    /**
-     * Processes a request from a Relying Party
-     * 
-     * @param OpenID_Message $message The OpenID_Message which was sent by the RP
-     * 
-     * @return bool true on success, false on failure
-     */
-    public function process(OpenID_Message $message)
+    public function process()
     {
+        switch ($this->message->get('openid.mode')) {
+        case 'associate':
+           $this->processAssociate();
+        case 'checkid_setup':
+           $this->processCheckIDSetup();
+        case 'checkid_authenticate':
+           $this->processCheckIDAuthenticate();
+        default:
+            throw new OpenID_Provider_Exception(
+                'Invalid mode'
+            );
+        }
     }
 
     public function getResponse()
