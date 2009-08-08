7c903f1b15ad4410e61208ba6db7c84f40175050
Bill Shupp <hostmaster@shupp.org>
2009-08-07 23:04:56 -0700

    Added OpenID_Discover_HTMLTest

---
+++ tests/OpenID/Discover/HTMLTest.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * OpenID_Discover_HTMLTest 
+ * 
+ * PHP Version 5.2.0+
+ * 
+ * @uses      PHPUnit_Framework_TestCase
+ * @category  Auth
+ * @package   OpenID
+ * @author    Bill Shupp <hostmaster@shupp.org> 
+ * @copyright 2009 Bill Shupp
+ * @license   http://www.opensource.org/licenses/bsd-license.php FreeBSD
+ * @link      http://pearopenid.googlecode.com
+ */
+
+require_once 'PHPUnit/Framework.php';
+require_once 'OpenID/Discover/HTML.php';
+
+/**
+ * OpenID_Discover_HTMLTest 
+ * 
+ * @uses      PHPUnit_Framework_TestCase
+ * @category  Auth
+ * @package   OpenID
+ * @author    Bill Shupp <hostmaster@shupp.org> 
+ * @copyright 2009 Bill Shupp
+ * @license   http://www.opensource.org/licenses/bsd-license.php FreeBSD
+ * @link      http://pearopenid.googlecode.com
+ */
+class OpenID_Discover_HTMLTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * testDiscoverSuccess 
+     * 
+     * @return void
+     */
+    public function testDiscoverSuccess()
+    {
+        $html = '<html>
+                 <head>
+                     <link rel="openid.server" href="http://www.example.com/server">
+                     <link rel="openid.delegate" href="http://user.example.com">
+                 </head>
+                 </html>';
+
+        $object = $this->getMock('OpenID_Discover_HTML',
+                                 array('sendRequest'),
+                                 array('http://example.com'));
+
+        $object->expects($this->once())
+               ->method('sendRequest')
+               ->will($this->returnValue($html));
+
+        $serviceEndpoints = $object->discover();
+        $this->assertType('OpenID_ServiceEndpoints', $serviceEndpoints);
+
+        // Version 2.0
+        $html = '<html>
+                 <head>
+                     <link rel="openid2.provider" href="http://example.com/server">
+                     <link rel="openid2.local_id" href="http://user.example.com">
+                 </head>
+                 </html>';
+
+        $object = $this->getMock('OpenID_Discover_HTML',
+                                 array('sendRequest'),
+                                 array('http://example.com'));
+
+        $object->expects($this->once())
+               ->method('sendRequest')
+               ->will($this->returnValue($html));
+
+        $serviceEndpoints = $object->discover();
+        $this->assertType('OpenID_ServiceEndpoints', $serviceEndpoints);
+
+        // Directed Identity
+        $html = '<html>
+                 <head>
+                     <link rel="openid2.provider" href="http://example.com/server">
+                 </head>
+                 </html>';
+
+        $object = $this->getMock('OpenID_Discover_HTML',
+                                 array('sendRequest'),
+                                 array('http://example.com'));
+
+        $object->expects($this->once())
+               ->method('sendRequest')
+               ->will($this->returnValue($html));
+
+        $serviceEndpoints = $object->discover();
+        $this->assertType('OpenID_ServiceEndpoints', $serviceEndpoints);
+    }
+
+    /**
+     * testDiscoverFail 
+     * 
+     * @expectedException OpenID_Discover_Exception
+     * @return void
+     */
+    public function testDiscoverFail()
+    {
+        $html = '<html>
+                 <head>
+                 </head>
+                 </html>';
+
+        $object = $this->getMock('OpenID_Discover_HTML',
+                                 array('sendRequest'),
+                                 array('http://example.com'));
+
+        $object->expects($this->once())
+               ->method('sendRequest')
+               ->will($this->returnValue($html));
+
+        $serviceEndpoints = $object->discover();
+        $this->assertType('OpenID_ServiceEndpoints', $serviceEndpoints);
+    }
+}
+?>
