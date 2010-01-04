4d81abf79235f1ce04556029986b88510d90b6b2
Bill Shupp <hostmaster@shupp.org>
2010-01-04 14:01:10 -0800

    Added jquery to RP examples to that you can toggle the OAuth attributes

--- examples/common/rp_form.php
+++ examples/common/rp_form.php
@@ -1,3 +1,14 @@
+<script type="text/javascript" src="selector/js/jquery-1.2.6.min.js"></script>
+<script type="text/javascript">
+function toggleOAuth() {
+    $('#oauth-scope').toggle();
+    $('#oauth-consumer-key').toggle();
+    $('#oauth-consumer-secret').toggle();
+    $('#oauth-access-token-url').toggle();
+    $('#oauth-access-token-method').toggle();
+}
+</script>
+
     <form action="./relyingparty.php" method="POST">
     <table border="0">
         <tr><td>OpenID URL: </td><td><input type="text" name="identifier"></td></tr>
@@ -6,7 +17,7 @@
         <tr><td>Simple Reg: </td><td><input type="checkbox" name="sreg"></td></tr>
         <tr><td>AX: </td><td><input type="checkbox" name="ax"> </td></tr>
         <!-- <tr><td>UI: </td><td><input type="checkbox" name="ui"> </td></tr> -->
-        <tr><td>OAuth: </td><td><input type="checkbox" name="oauth" onclick="document.getElementById('oauth-scope').style.display = '';document.getElementById('oauth-consumer-key').style.display = '';document.getElementById('oauth-consumer-secret').style.display = '';document.getElementById('oauth-access-token-url').style.display = '';document.getElementById('oauth-access-token-method').style.display = '';"> </td></tr>
+        <tr><td>OAuth: </td><td><input type="checkbox" name="oauth" onclick="toggleOAuth()"> </td></tr>
         <tr style="display: none;" id="oauth-scope"><td>OAuth Scope: </td><td><input type="text" name="oauth_scope"> </td></tr>
         <tr style="display: none;" id="oauth-consumer-key"><td>OAuth Consumer Key: </td><td><input type="text" name="oauth_consumer_key"> </td></tr>
         <tr style="display: none;" id="oauth-consumer-secret"><td>OAuth Consumer Secret: </td><td><input type="text" name="oauth_consumer_secret"> </td></tr>
