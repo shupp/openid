aa7ad8727173b1b7e93e1246eee90b2e8b1c86e1
Bill Shupp <hostmaster@shupp.org>
2010-03-08 23:07:33 -0800

    Added whitelisting to runTests.php, which exposes some missing code coverage.  I'll get that fixed soon.

--- runTests.php
+++ runTests.php
@@ -6,6 +6,8 @@ set_include_path(get_include_path() . ":{$base}:{$base}/tests");
 require_once 'PHPUnit/Util/Filter.php';
 
 PHPUnit_Util_Filter::addFileToFilter(__FILE__, 'PHPUNIT');
+PHPUnit_Util_Filter::addFileToWhitelist('OpenID.php');
+PHPUnit_Util_Filter::addDirectoryToWhitelist('OpenID');
 
 require_once 'PHPUnit/TextUI/Command.php';
 
